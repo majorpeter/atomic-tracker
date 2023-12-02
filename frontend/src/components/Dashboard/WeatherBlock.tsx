8cb28223e1ec62d9433ec1dc81673c48222531af
majorpeter <majorpeter29@gmail.com>
2023-12-02 22:35:01 +0100

    added weather-icons-react, started using it

--- frontend/src/components/Dashboard/WeatherBlock.tsx
+++ frontend/src/components/Dashboard/WeatherBlock.tsx
@@ -1,15 +1,30 @@
-import { Stack } from "@mui/joy";
+import { Stack, Typography } from "@mui/joy";
+
+import * as WeatherIcons from "weather-icons-react";
+
+import { Api } from "@api";
 import { useApiQuery_weather } from "../../util/api-client";
+
+function getWeatherSymbol(
+  symbol: Api.Weather.type["weather_symbol"]
+): React.JSX.Element {
+  switch (symbol) {
+    case "cloudy":
+      return <WeatherIcons.WiCloudy />;
+  }
+  return <WeatherIcons.WiAlien />;
+}
 
 const WeatherBlock: React.FC = () => {
   const { data } = useApiQuery_weather();
-  return (
-    <Stack>
-      {data?.temp_celsius}
-      <br />
-      {data?.weather_symbol}
-    </Stack>
-  );
+  if (data) {
+    return (
+      <Stack sx={{ ml: 2 }}>
+        {getWeatherSymbol(data.weather_symbol)}
+        <Typography>{data.temp_celsius} ˚C</Typography>
+      </Stack>
+    );
+  }
 };
 
 export default WeatherBlock;
